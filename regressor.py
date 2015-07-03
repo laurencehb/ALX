c2533da0c7158274bd2f798e375c66f4a21c2581
laurencehb <laurence.bonnet@gmail.com>
2015-07-03 17:26:37 +0200

    mon premier commit

--- regressor.py
+++ regressor.py
@@ -3,7 +3,7 @@ from sklearn.base import BaseEstimator
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = RandomForestRegressor(n_estimators=10, max_depth=10, max_features=10)
+        self.clf = RandomForestRegressor(n_estimators=2.7, max_depth=1.9, max_features=10)
 
     def fit(self, X, y):
         self.clf.fit(X, y)
