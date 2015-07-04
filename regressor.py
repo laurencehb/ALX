a17d7ff473460bc769226bfa246646ebec19bec8
laurencehb <laurence.bonnet@gmail.com>
2015-07-04 16:00:50 +0200

    last chance

--- regressor.py
+++ regressor.py
@@ -1,15 +1,24 @@
-from sklearn.ensemble import RandomForestRegressor
+        
 from sklearn.base import BaseEstimator
-from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import BaggingRegressor, RandomForestRegressor
+from sklearn.ensemble import ExtraTreesRegressor, AdaBoostRegressor, GradientBoostingRegressor
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        cl = RandomForestRegressor(n_estimators=31 , max_depth=25, max_features=20)
-        self.clf = AdaBoostRegressor(base_estimator = cl, n_estimators=39)
+ #       cl = RandomForestRegressor(n_estimators=31, max_depth=25, max_features=20, n_jobs=4)
+#base_estimator = cl
+        self.clf1 = ExtraTreesRegressor(n_estimators=500, max_depth = 10, bootstrap = True, n_jobs= 7)
 
+        self.clf2 = BaggingRegressor(n_estimators=500, max_features=10, bootstrap=True, bootstrap_features=True, n_jobs=7)
+        cl = RandomForestRegressor(n_estimators=31, max_depth=25, max_features=20, n_jobs=7)
+        self.clf3 = AdaBoostRegressor(base_estimator = cl, n_estimators=39)
+        self.clf4 = GradientBoostingRegressor(n_estimators = 500, max_depth = 25, max_features = 20)
     def fit(self, X, y):
-        self.clf.fit(X, y)
-
+        self.clf1.fit(X, y)
+        self.clf2.fit(X, y)
+        self.clf3.fit(X, y)
+        self.clf4.fit(X, y)
+    
     def predict(self, X):
-        return self.clf.predict(X)
-#RandomForestClassifier
+        return (self.clf1.predict(X) +self.clf2.predict(X) +self.clf3.predict(X) +self.clf4.predict(X))/4
+        
