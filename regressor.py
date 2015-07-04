cf880ab7a22443de6e074b76b1126c79d634fcd5
laurencehb <laurence.bonnet@gmail.com>
2015-07-04 10:33:34 +0200

    AB_RFok

--- regressor.py
+++ regressor.py
@@ -4,7 +4,7 @@ from sklearn.ensemble import AdaBoostRegressor
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        cl = RandomForestRegressor(n_estimators=2.7, max_depth=1.9, max_features=10)
+        cl = RandomForestRegressor(n_estimators=27, max_depth=19, max_features=10)
         self.clf = AdaBoostRegressor(base_estimator = cl, n_estimators=100)
 
     def fit(self, X, y):
